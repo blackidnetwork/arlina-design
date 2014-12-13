96fd9d592153fc5815b301752d69383301aff8f8
melani.find96@gmail.com <melani.find96@gmail.com@f17e53d1-1a3b-cfba-96d2-677222e0e814>
2014-12-13 21:25:57 +0000

    ss

--- javascript/viewer.js
+++ javascript/viewer.js
@@ -9,7 +9,7 @@ $('a[href$=bmp]:has(img)').lightBox();
 
 //Hide
 $(document)['ready'](function () {
-        $('#cpright')['html']('<a href="http://arlinadesign.blogspot.com">Arlina Design</a>');
+        $('#cpright')['html']('Created by <a href="http://arlinadesign.blogspot.com">Arlina Design</a>');
         setInterval(function () {
                 if (!$('#cpright:visible')['length']) {
                     window['location']['href'] = 'http://arlinadesign.blogspot.com';
