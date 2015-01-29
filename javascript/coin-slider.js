752356f1ff34357cab9365bc0bd091981f3aa46e
melani.find96@gmail.com <melani.find96@gmail.com@f17e53d1-1a3b-cfba-96d2-677222e0e814>
2015-01-29 01:24:35 +0000

    cs

--- javascript/coin-slider.js
+++ javascript/coin-slider.js
@@ -471,13 +471,13 @@
 	$.fn.coinslider.defaults = {	
 		width: 565, // width of slider panel
 		height: 290, // height of slider panel
-		spw: 7, // squares per width
-		sph: 5, // squares per height
+		spw: 1, // squares per width
+		sph: 1, // squares per height
 		delay: 3000, // delay between images in ms
 		sDelay: 30, // delay beetwen squares in ms
 		opacity: 0.7, // opacity of title and navigation
 		titleSpeed: 500, // speed of title appereance in ms
-		effect: '', // random, swirl, rain, straight
+		effect: 'rain', // random, swirl, rain, straight
 		navigation: true, // prev next and buttons
 		links : true, // show images as links 
 		hoverPause: true // pause on hover		
